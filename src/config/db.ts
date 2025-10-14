bb9f434c6a1cefb81c1b1ac9c41a4675a46016df
João Pedro <jpsousa471@gmail.com>
2025-10-14 19:16:48 -0300

    first commit

---
+++ src/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { env } from './env';
+
+const connectDB = async () => {
+  const { HOST_USERNAME, HOST_DATABASE, HOST_CLUSTER, HOST_PASSWORD } = env;
+
+  const URI = `mongodb+srv://${HOST_USERNAME}:${HOST_PASSWORD}@${HOST_CLUSTER}.znlxk8q.mongodb.net/${HOST_DATABASE}?retryWrites=true&w=major`;
+
+  try {
+    await mongoose.connect(URI);
+    console.log('Connect to the MongoDB');
+  } catch (error: any) {
+    console.error(`Error connecting to MongoDB: ${error.message}.`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
