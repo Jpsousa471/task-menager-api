6cc25880bdbd39b567f00cfc4a3964e9ae0a1941
João Pedro <jpsousa471@gmail.com>
2025-11-01 17:08:36 -0300

    remocao da pasta dist

--- dist/schemas/update-task-schema.js
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-import { createTaskSchema } from './create-task-schema.js';
-export const updateTaskParamsSchema = z.object({
-    id: z.string().min(24, { message: 'O id deve conter 24 caracteres.' })
-});
-export const updateTaskBodySchema = z.object({ ...createTaskSchema.shape });
